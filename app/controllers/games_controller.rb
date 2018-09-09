3bc5d327fa6ad402d82cf495ae23f244ce2c1e3c
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 08:20:52 -0500

    add method routes and views new and play

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,10 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new 
+  end 
+
+  def play
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'games#index'
   get 'games/index'
+  get 'games/new'
+  get 'games/play'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
