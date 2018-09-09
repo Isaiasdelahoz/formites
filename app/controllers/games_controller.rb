9c273591990eb2a22d343671bb1bdd3cda4f73a2
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 10:17:23 -0500

    add route attack and action attack

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'game'
+
 class GamesController < ApplicationController
   def index
   end
@@ -7,14 +9,15 @@ class GamesController < ApplicationController
 
   def create
     session[:nickname1] = params[:nickname1]
-    session[:nickname2] = params[:nickname2]
-    session[:score_1] = 0
-    session[:score_2] = 0
+    session[:nickname1] = params[:nickname2]
+
     redirect_to games_play_path
   end
 
   def play
-    session[:nickname1]
-    session[:score_1]
+    g1 = Game.new(session[:nickname1], session[:nickname2])
+  end
+  
+  def attack
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'games#index'
   
-  resources :games, only: [:index, :new, :create]
-  
-  get 'games/play'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games, only: [:index, :new, :create] do
+    collection do
+      get :play
+      put :attack
+    end
+
+  end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
