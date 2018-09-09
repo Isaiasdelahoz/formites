9a705c27a1eace4dedf6b534fdf421d324b0b975
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 07:42:08 -0500

    fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  root 'games#index'
-  resources :games
-  get 'games/play'
-  # get 'games/show'
-  # get 'games/new'
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
