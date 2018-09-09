94cefffaebf23d13833034fc8bb1b9b387e5944d
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 08:50:40 -0500

    add session to method create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,18 @@ class GamesController < ApplicationController
   end
 
   def new 
-  end 
+  end
+
+  def create
+    session[:nickname1] = params[:nickname1]
+    session[:nickname2] = params[:nickname2]
+    session[:score_1] = 0
+    session[:score_2] = 0
+    redirect_to games_play_path
+  end
 
   def play
+    session[:nickname1]
+    session[:score_1]
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'games#index'
-  get 'games/index'
-  get 'games/new'
+  
+  resources :games, only: [:index, :new, :create]
+  
   get 'games/play'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
