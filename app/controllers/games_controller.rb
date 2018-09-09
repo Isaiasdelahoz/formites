b9e943910e97ef0713d125345b780fc8fe86ac37
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 07:31:48 -0500

    added form_Tag and play view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,25 @@
 class GamesController < ApplicationController
   def index
-  end
-
-  def show
+    @user1 = 2
   end
 
   def new
   end
+
+  def create
+    @name = session[:nickname1] = params[:nickname1]
+    session[:nickname2] = params[:nickname2]
+    session[:points_1] = 0
+    session[:points_2] = 0
+    redirect_to games_play_path
+  end
+
+  def show
+    @name
+  end
+
+
+  def play 
+    @user1
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'games#index'
-  get 'games/index'
-  get 'games/show'
-  get 'games/new'
+  resources :games
+  get 'games/play'
+  # get 'games/show'
+  # get 'games/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
