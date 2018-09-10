736b4e9094f105409dfa286f552c075e49ace906
Isaias De La Hoz <isaiasdelahoz@gmail.com>
2018-09-09 20:22:49 -0500

    add default score to model user

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_09_10_002515) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
-    t.integer "score"
+    t.integer "score", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
